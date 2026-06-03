b5b3e47e87d36ba887b8d67ad147a323a03b228f
Ale8045 <tutuh8618@gmail.com>
2026-06-03 10:28:56 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 curl -L --connect-timeout 15 --retry 3 \
-https://raw.githubusercontent.com/Ale8045/hy2-manager/main/hy2.sh \
+"https://raw.githubusercontent.com/Ale8045/hy2-manager/main/hy2.sh" \
 -o /usr/local/bin/hy2
 
-# 自动修复Windows换行符
+# 修复 Windows CRLF
 sed -i 's/\r$//' /usr/local/bin/hy2
 
 chmod +x /usr/local/bin/hy2
 
 echo "HY2 安装完成"
 
-hy2
+exec /usr/local/bin/hy2
