93bfc1e5c6435458d32a528f51eb641b2555a68e
Ale8045 <tutuh8618@gmail.com>
2026-06-03 10:23:31 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-URLS=(
-"https://raw.githubusercontent.com/Ale8045/hy2-manager/main/hy2.sh"
-"https://gh.llkk.cc/https://raw.githubusercontent.com/Ale8045/hy2-manager/main/hy2.sh"
-"https://gh-proxy.com/https://raw.githubusercontent.com/Ale8045/hy2-manager/main/hy2.sh"
-)
+curl -fsSL \
+https://raw.githubusercontent.com/Ale8045/hy2-manager/main/hy2.sh \
+-o /usr/local/bin/hy2
 
-for url in "${URLS[@]}"; do
-  echo "正在尝试下载: $url"
-  if curl -L --connect-timeout 10 --retry 2 "$url" -o /usr/local/bin/hy2; then
-    break
-  fi
-done
-
-sed -i 's/\r$//' /usr/local/bin/hy2
 chmod +x /usr/local/bin/hy2
 
-echo "HY2 管理脚本安装完成"
+echo "HY2 安装完成"
 
 hy2
